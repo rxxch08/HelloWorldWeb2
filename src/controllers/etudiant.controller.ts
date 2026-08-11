07aa18dab1d2444e7c1e0f7bbea1c7d75ab8392f
Thimy Ndratontseheno <hei.thimy@gmail.com>
2026-08-11 10:34:56 +0300

    Ajout des controllers

--- src/controllers/etudiant.controller.ts
+++ src/controllers/etudiant.controller.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
+import { ApiError } from '../utils/ApiError'
 import { getAll, getById, create, update, partialUpdate, remove } from '../models/etudiant.model'
 
 export const getAllEtudiants = (req: Request, res: Response) => {
@@ -17,51 +18,51 @@ export const getEtudiantById = (req: Request, res: Response) => {
   res.status(200).json(etudiant)
 }
 
-export const createEtudiant = (req: Request, res: Response) => {
+export const createEtudiant = (req: Request, res: Response, next: NextFunction) => {
   const { nom, prenom, email } = req.body
 
   if (!nom || !prenom || !email) {
-    return res.status(400).json({ message: 'Les champs nom, prenom et email sont requis' })
+    return next(new ApiError(400, 'Les champs nom, prenom et email sont requis'))
   }
 
   const nouvelEtudiant = create(nom, prenom, email)
   res.status(201).json(nouvelEtudiant)
 }
 
-export const updateEtudiant = (req: Request, res: Response) => {
+export const updateEtudiant = (req: Request, res: Response, next: NextFunction) => {
   const id = Number(req.params.id)
   const { nom, prenom, email } = req.body
 
   if (!nom || !prenom || !email) {
-    return res.status(400).json({ message: 'Les champs nom, prenom et email sont requis' })
+    return next(new ApiError(400, 'Les champs nom, prenom et email sont requis'))
   }
 
   const etudiant = update(id, nom, prenom, email)
 
   if (!etudiant) {
-    return res.status(404).json({ message: 'Étudiant non trouvé' })
+    return next(new ApiError(404, 'Étudiant non trouvé'))
   }
 
   res.status(200).json(etudiant)
 }
 
-export const partialUpdateEtudiant = (req: Request, res: Response) => {
+export const partialUpdateEtudiant = (req: Request, res: Response, next: NextFunction) => {
   const id = Number(req.params.id)
   const etudiant = partialUpdate(id, req.body)
 
   if (!etudiant) {
-    return res.status(404).json({ message: 'Étudiant non trouvé' })
+    return next(new ApiError(404, 'Étudiant non trouvé'))
   }
 
   res.status(200).json(etudiant)
 }
 
-export const deleteEtudiant = (req: Request, res: Response) => {
+export const deleteEtudiant = (req: Request, res: Response, next: NextFunction) => {
   const id = Number(req.params.id)
   const success = remove(id)
 
   if (!success) {
-    return res.status(404).json({ message: 'Étudiant non trouvé' })
+    return next(new ApiError(404, 'Étudiant non trouvé'))
   }
 
   res.status(204).send()
